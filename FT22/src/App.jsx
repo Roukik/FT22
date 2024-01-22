93667a32f08868cf2af0661f7c65a9c1b6ee5518
Roukik <148196386+Roukik@users.noreply.github.com>
2024-01-22 19:02:49 +0100

    .

--- FT22/src/App.jsx
+++ FT22/src/App.jsx
@@ -9,16 +9,23 @@ function App() {
   return (
 
         <div className="conteneur">
-          <div className="partie-blanche"></div>
+
+          <div className="partie-blanche">
+            <img scr="FT22/"
+
+          </div>
+
           <div className="partie-grise">
             <div id="export">
             <button class="export-button">Export</button>
             </div>
           </div>
+
           <div className="partie-bleue">
             <div id="export1">
             <button class="export1-button">Export</button>
             </div>
+
           </div>
         </div>
       );
