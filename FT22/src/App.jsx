5d554e121f248a81ef1f1426589af8b9142f9913
Roukik <148196386+Roukik@users.noreply.github.com>
2024-01-24 16:06:45 +0100

    .

--- FT22/src/App.jsx
+++ FT22/src/App.jsx
@@ -1,36 +1,55 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import {Bar} from "react-chartjs-2"
+
+const data = {
+  labels: ["red", "bleu", "green", "violet", "orange", "yellow"],
+  datasets: [
+    {
+      label: "couleurs préférées en France",
+      data: [23, 21, 32, 12, 15, 9],
+      backgroundColor: ["red", "blue", "green", "violet", "orange", "yellow"],
+    },
+  ],
+};
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
   return (
-
-        <div className="conteneur">
-
-          <div className="partie-blanche">
-            <img src="FT22/FT22/Images/logo_sncfbleu.png" alt="" className="logo" />
-          </div>
-
-          <div className="partie-grise">
-            <div id="export">
-            <button class="export-button">Export</button>
-            
+    <div className="conteneur">
+      <div className="partie-blanche">
+        <img src="../Images/logo_sncfbleu.png" alt="" className="logo" />
+      </div>
+      <div className="partie-grise">
+        <div id="export">
+          <button className="export-button">Export</button>
+        </div>
+                <h2 className='moisgauche'>Octobre 2023</h2>
+        <div className="App">
+          <div className="container">
+            <div className="card">
+              <Bar data={data} /> {}
             </div>
-          </div>
-
-          <div className="partie-bleue">
-            <div id="export1">
-            <button class="export1-button">Export</button>
-            </div>
-
           </div>
         </div>
-
-      );
-      
-      
+      </div>
+      <div className="partie-bleue">
+      <h2 className='annuel'>Annuel</h2>
+        <div id="export1">
+          <button className="export1-button">Export</button>
+        </div>
+        <div className="App">
+          <div className="container">
+            <div className="card">
+              <Bar data={data} /> {}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
+
